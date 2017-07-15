ceb85a0c6795d71ec32e88a6600edd955aeb11a5
PrinzJuliano <julian@pjog.de>
2017-07-15 13:34:27 +0200

    Bug fixes!
    Added a general License (MIT) to ensure no rights can be claimed on the
    images!
    Added a pop up to display set license.
    
    Reflow / Reindent of the files

--- js/scripts.js
+++ js/scripts.js
@@ -95,21 +95,20 @@ function displayPictures(){
 		image1.style.width = "100%"
 		image1.style.height = "100%"
 		$('#pictureRow' + i).append("<div class='col-md-6'>"+
-										"<div class='panel panel-default'>"+
-											"<div class='panel-heading'>"+
-												"<h3 class='panel-title'>"+
-													pictures[i].title +
-												"</h3>"+
-											"</div>"+
-											"<div class='panel-body' id='pictureFrame"+i+"'>"+
-											"</div>"+
-											"<div class='panel-footer clearfix'>"+
-												"<p class='pull-left'>By " + pictures[i].author + "</p>" +
-												"<a href='"+pictures[i].data+"' target='_blank' class='btn btn-primary pull-right'>Download</button>"+
-											"</div>"+
-										"</div>"+
-									"</div>"
-									);
+			"<div class='panel panel-default'>"+
+			"<div class='panel-heading'>"+
+			"<h3 class='panel-title'>"+
+			pictures[i].title +
+			"</h3>"+
+			"</div>"+
+			"<div class='panel-body' id='pictureFrame"+i+"'>"+
+			"</div>"+
+			"<div class='panel-footer clearfix'>"+
+			"<p class='pull-left'>By " + pictures[i].author + "</p>" +
+			"<a href='"+pictures[i].data+"' target='_blank' class='btn btn-primary pull-right'>Download</button>"+
+			"</div>"+
+			"</div>"+
+			"</div>");
 		$('#pictureFrame' + i).append(image1);
 
 		if((i+1) < pictures.length){
@@ -118,21 +117,20 @@ function displayPictures(){
 			image2.style.width = "100%"
 			image2.style.height = "100%"
 			$('#pictureRow' + i).append("<div class='col-md-6'>"+
-											"<div class='panel panel-default'>"+
-												"<div class='panel-heading'>"+
-													"<h3 class='panel-title'>"+
-														pictures[i+1].title +
-													"</h3>"+
-												"</div>"+
-												"<div class='panel-body' id='pictureFrame"+(i+1)+"'>"+
-												"</div>"+
-												"<div class='panel-footer clearfix'>"+
-													"<p class='pull-left'>By " + pictures[i+1].author + "</p>" +
-													"<a href='"+pictures[i+1].data+"' target='_blank' class='btn btn-primary pull-right'>Download</button>"+
-												"</div>"+
-											"</div>"+
-										"</div>"
-										);
+				"<div class='panel panel-default'>"+
+				"<div class='panel-heading'>"+
+				"<h3 class='panel-title'>"+
+				pictures[i+1].title +
+				"</h3>"+
+				"</div>"+
+				"<div class='panel-body' id='pictureFrame"+(i+1)+"'>"+
+				"</div>"+
+				"<div class='panel-footer clearfix'>"+
+				"<p class='pull-left'>By " + pictures[i+1].author + "</p>" +
+				"<a href='"+pictures[i+1].data+"' target='_blank' class='btn btn-primary pull-right'>Download</button>"+
+				"</div>"+
+				"</div>"+
+				"</div>");
 			$('#pictureFrame' + (i+1)).append(image2);
 		}
 	}
@@ -140,7 +138,7 @@ function displayPictures(){
 
 function upload(){
 
-		if (window.File && window.FileReader && window.FileList && window.Blob) {
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
 	  // Great success! All the File APIs are supported.
 	} else {
 		alert('The File APIs are not fully supported in this browser.');
@@ -166,7 +164,6 @@ function upload(){
 	}
 
 	var metadata = {
-		name : (account.displayName || account.email) + "_" + title + "_" + file.name,
 		author: (account.displayName || account.email),
 		title: title,
 		type: "pending"
@@ -187,123 +184,141 @@ function upload(){
 }
 
 function getBase64(file, metadata) {
-   var reader = new FileReader();
-   reader.readAsDataURL(file);
-   reader.onload = function () {
-   		metadata.data = reader.result;
+	var reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = function () {
+		metadata.data = reader.result;
    		//do the work
-		var key = database.ref().child("files").push().key
-		database.ref("files/" + key).set(metadata).then(function(){
+   		var key = database.ref().child("files").push().key
+   		database.ref("files/" + key).set(metadata).then(function(){
 
-			$('#upload_title').val("")
-			$('#files').val("")
+   			$('#upload_title').val("")
+   			$('#files').val("")
 
-			$('#uploadModal').modal("hide")
-		});
-   };
-   reader.onerror = function (error) {
-     if(!$("#files").parent().hasClass("has-error")){
-			$('#files').parent().addClass("has-error")
-			$('#files').parent().append("<span id='helpTextUpload' class='help-block'>"+error+"</span>")
-		} else {
-			$('#helpTextUpload').html(error);
-		}
-		return
-     return -1;
-   };
-
-
+   			$('#uploadModal').modal("hide")
+   		});
+   	};
+   	reader.onerror = function (error) {
+   		if(!$("#files").parent().hasClass("has-error")){
+   			$('#files').parent().addClass("has-error")
+   			$('#files').parent().append("<span id='helpTextUpload' class='help-block'>"+error+"</span>")
+   		} else {
+   			$('#helpTextUpload').html(error);
+   		}
+   		return
+   		return -1;
+   	};
 }
 
 
 /**
 	Login functions
-	*/
+*/
 
-	function googleLogin(){
-		$('#loginModal').modal('hide')
+function googleLogin(){
+	$('#loginModal').modal('hide')
 
-		var provider = new firebase.auth.GoogleAuthProvider();
+	var provider = new firebase.auth.GoogleAuthProvider();
 
-		firebase.auth().signInWithRedirect(provider);
+	firebase.auth().signInWithRedirect(provider);
 
-	}
+}
 
-	function emailLoginModal(){
-		$('#loginModal').modal('hide')
-		$('#emailModal').modal('show')
-	}
+function emailLoginModal(){
+	$('#loginModal').modal('hide')
+	$('#emailModal').modal('show')
+}
 
-	function createUserModal(){
-		$('#emailModal').modal('hide')
-		$('#createUserModal').modal('show')
-	}
+function createUserModal(){
+	$('#emailModal').modal('hide')
+	$('#createUserModal').modal('show')
+}
 
-	function emailUserAuthModal(){
-		$('#emailModal').modal('hide')
-		$('#emailUserAuthModal').modal('show')
-	}
+function emailUserAuthModal(){
+	$('#emailModal').modal('hide')
+	$('#emailUserAuthModal').modal('show')
+}
 
-	function signUp(){
-		var email = $('#create_email').val()
-		var password1 = $('#create_password').val()
-		var password2 = $('#create_password2').val()
+function signUp(){
+	var email = $('#create_email').val()
+	var password1 = $('#create_password').val()
+	var password2 = $('#create_password2').val()
 
-		var verified = true
+	var verified = true
 
-		if(email.isEmpty())
+	if(email.isEmpty())
+	{
+		if(!$('#create_email').parent().hasClass("has-error")){
+			$('#create_email').parent().addClass("has-error")
+			$('#create_email').parent().append('<span id="helpBlock1" class="help-block">Missing email!</span>')
+		} else {
+			$('#helpBlock1').html("Missing email!")
+		}
+		verified = verified & false
+	} else {
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+		if(re.test(email))
 		{
+			if($('#create_email').parent().hasClass("has-error")){
+				$('#create_email').parent().removeClass("has-error")
+				$('#helpBlock1').remove()
+			}
+
+			$('#create_email').parent().addClass("has-success")
+			verified = verified & true;
+		} else {
 			if(!$('#create_email').parent().hasClass("has-error")){
 				$('#create_email').parent().addClass("has-error")
-				$('#create_email').parent().append('<span id="helpBlock1" class="help-block">Missing email!</span>')
+				$('#create_email').parent().append('<span id="helpBlock1" class="help-block">This is not a correct email!</span>')
 			} else {
-				$('#helpBlock1').html("Missing email!")
-			}
-			verified = verified & false
-		} else {
-			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-			if(re.test(email))
-			{
-				if($('#create_email').parent().hasClass("has-error")){
-					$('#create_email').parent().removeClass("has-error")
-					$('#helpBlock1').remove()
-				}
-
-				$('#create_email').parent().addClass("has-success")
-				verified = verified & true;
-			} else {
-				if(!$('#create_email').parent().hasClass("has-error")){
-					$('#create_email').parent().addClass("has-error")
-					$('#create_email').parent().append('<span id="helpBlock1" class="help-block">This is not a correct email!</span>')
-				} else {
-					$('#helpBlock1').html("This is not a correct email!")
-				}
-
-				verified = verified & false;
-			}
-		}
-
-		if(password1.isEmpty())
-		{
-			if(!$('#create_password').parent().hasClass("has-error"))
-			{
-				$('#create_password').parent().addClass("has-error")
-				$('#create_password').parent().append('<span id="helpBlock2" class="help-block">Missing password!</span>')
-			} else 
-			{
-				$('#helpBlock2').html("Missing Password!");
+				$('#helpBlock1').html("This is not a correct email!")
 			}
 
 			verified = verified & false;
-		} else if(password2.isEmpty()){
+		}
+	}
 
-			if($('#create_password').parent().hasClass("has-error"))
-			{
-				$('#create_password').parent().removeClass("has-error")
-				$('#helpBlock2').remove()
-			}
+	if(password1.isEmpty())
+	{
+		if(!$('#create_password').parent().hasClass("has-error"))
+		{
+			$('#create_password').parent().addClass("has-error")
+			$('#create_password').parent().append('<span id="helpBlock2" class="help-block">Missing password!</span>')
+		} else 
+		{
+			$('#helpBlock2').html("Missing Password!");
+		}
 
+		verified = verified & false;
+	} else if(password2.isEmpty()){
+
+		if($('#create_password').parent().hasClass("has-error"))
+		{
+			$('#create_password').parent().removeClass("has-error")
+			$('#helpBlock2').remove()
+		}
+
+		if(!$('#create_password2').parent().hasClass("has-error"))
+		{
+			$('#create_password2').parent().addClass("has-error")
+			$('#create_password2').parent().append('<span id="helpBlock3" class="help-block">The passwords are not equal!</span>')
+		} else 
+		{
+			$('#helpBlock3').html("The passwords are not equal!");
+		}
+
+		verified = verified & false;
+
+	} else {
+		if($('#create_password').parent().hasClass("has-error"))
+		{
+			$('#create_password').parent().removeClass("has-error")
+			$('#helpBlock2').remove()
+		}
+
+		if(password1 != password2)
+		{
 			if(!$('#create_password2').parent().hasClass("has-error"))
 			{
 				$('#create_password2').parent().addClass("has-error")
@@ -314,77 +329,57 @@ function getBase64(file, metadata) {
 			}
 
 			verified = verified & false;
-
 		} else {
-			if($('#create_password').parent().hasClass("has-error"))
+			if($('#create_password2').parent().hasClass("has-error"))
 			{
-				$('#create_password').parent().removeClass("has-error")
-				$('#helpBlock2').remove()
+				$('#create_password2').parent().removeClass("has-error")
+				$('#helpBlock3').remove()
 			}
 
-			if(password1 != password2)
-			{
-				if(!$('#create_password2').parent().hasClass("has-error"))
-				{
-					$('#create_password2').parent().addClass("has-error")
-					$('#create_password2').parent().append('<span id="helpBlock3" class="help-block">The passwords are not equal!</span>')
-				} else 
-				{
-					$('#helpBlock3').html("The passwords are not equal!");
-				}
-
-				verified = verified & false;
-			} else {
-				if($('#create_password2').parent().hasClass("has-error"))
-				{
-					$('#create_password2').parent().removeClass("has-error")
-					$('#helpBlock3').remove()
-				}
-
-				$('#create_password').parent().addClass("has-success")
-				$('#create_password2').parent().addClass("has-success")
-			}
+			$('#create_password').parent().addClass("has-success")
+			$('#create_password2').parent().addClass("has-success")
 		}
+	}
 
-		if(verified)
-		{
-			firebase.auth().createUserWithEmailAndPassword(email, password1).then(function(){
-				$('#createUserModal').modal("hide")
-			}).catch(function(error) {
+	if(verified)
+	{
+		firebase.auth().createUserWithEmailAndPassword(email, password1).then(function(){
+			$('#createUserModal').modal("hide")
+		}).catch(function(error) {
 		  // Handle Errors here.
 		  var errorCode = error.code;
 		  var errorMessage = error.message;
 		  alert(errorCode + ":\n" + errorMessage)
 		});
-		}
 	}
+}
 
-	function emailAuth(){
+function emailAuth(){
 
-		var email = $('#login_email').val()
-		var password = $('#login_password').val()
+	var email = $('#login_email').val()
+	var password = $('#login_password').val()
 
-		firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
-			$('#emailUserAuthModal').modal("hide")
-		}).catch(function(error) {
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+		$('#emailUserAuthModal').modal("hide")
+	}).catch(function(error) {
 	  // Handle Errors here.
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
 	  alert(errorCode + ":\n" + errorMessage)
 	});
-	}
+}
 
-	function logout(){
-		firebase.auth().signOut().then(function() {
-			console.log("Signed out!");
-		}).catch(function(error) {
-			console.log(error);
-		});
-	}
+function logout(){
+	firebase.auth().signOut().then(function() {
+		console.log("Signed out!");
+	}).catch(function(error) {
+		console.log(error);
+	});
+}
 
 /**
 	Prototypes
-	*/
-	String.prototype.isEmpty = function() {
-		return (this.length === 0 || !this.trim());
-	};
+*/
+String.prototype.isEmpty = function() {
+	return (this.length === 0 || !this.trim());
+};
